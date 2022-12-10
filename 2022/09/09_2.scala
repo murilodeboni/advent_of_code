6f91b6b00ec93a5519f4ba1ec3d5d2194d422090
Murilo Deboni <murilodeboni@Murilos-MacBook-Pro.local>
2022-12-10 18:41:34 +0100

    Working part 2

--- 2022/09/09_2.scala
+++ 2022/09/09_2.scala
@@ -5,52 +5,70 @@ val input = Source.fromFile("input.txt").getLines.toList.map(x => {
   (l(0),l(1).toInt)
 })
 
-def tailMove(oldRope:List[(Int,Int)], newRope:List[(Int,Int)]):List[(Int,Int)] = {
-  if (oldRope == List()) { newRope } else {
-  val h = newRope.last
-  val t = oldRope.head
+val initialRope = List.fill(10)(0,0)
 
-  val horDiff = h._1 - t._1
-  val verDiff = h._2 - t._2
+def moveTail(h:(Int,Int),t:(Int,Int)):(Int,Int) = {
+  val hx = h._1
+  val hy = h._2
+  val tx = t._1
+  val ty = t._2
 
-  val newTail = (horDiff,verDiff) match {
-    case (2,2) => (t._1+1,t._2+1)
-    case (-2,-2) => (t._1-1,t._2-1)
-    case (2,_) => (t._1+1,h._2)
-    case (-2,_) => (t._1-1,h._2)
-    case (_,2) => (h._1, t._2+1)
-    case (_,-2) => (h._1, t._2-1)
-    case _ => {
-      println(h,t)
-      t
-    }
-  }
-  tailMove(oldRope.drop(1), newRope :+ newTail)
-}}
+  val dx = hx - tx
+  val dy = hy - ty
 
-def moveHead(input:List[(String,Int)], rope:List[(Int,Int)] = List.fill(10)(0,0), hist: List[(Int, Int)] = List()): Int = {
-  input.headOption match {
-    case None => {
-      hist.toSet.size
-    }
-    case Some(i) => {
-      val h = rope.head
-      val newHead = i._1 match {
-        case "R" => (h._1 + 1, h._2)
-        case "L" => (h._1 - 1, h._2)
-        case "U" => (h._1, h._2 + 1)
-        case "D" => (h._1, h._2 - 1)
-      }
-      if (i._2 > 0) {
-        val newRope = tailMove(rope.tail,List(newHead))
-        moveHead(List((i._1,i._2-1)) ++ input.tail, newRope, hist :+ newRope.last) 
-      } else {
-        moveHead(input.tail, rope,  hist) 
-      }
-    }
+  if (math.abs(dx) < 2 && math.abs(dy) < 2) {
+    t
+  } else if (math.abs(dx) > 1 && math.abs(dy) > 1) {
+    (if (tx < hx) hx - 1 else hx + 1,if (ty < hy) hy - 1 else hy + 1)
+  } else if (math.abs(dx) > 1) {
+    (if (tx < hx) hx - 1 else hx + 1, hy)
+  } else if (math.abs(dy) > 1) {
+    (hx,if (ty < hy) hy - 1 else hy + 1)
+  } else {
+    t
   }
 }
 
-println(
-  moveHead(input)
-)
+var hist: List[(Int,Int)] = List()
+
+def bringTailToHead(rope: List[(Int,Int)],newRope: List[(Int,Int)]): List[(Int,Int)] = {
+  rope match {
+    case List() => {
+      // println(newRope)
+      hist = hist :+ newRope.last
+      newRope
+    }
+    case _ => bringTailToHead(rope.drop(1), newRope :+ moveTail(newRope.last,rope.head))
+  }
+}
+
+def moveHead(h:(Int,Int), rope: List[(Int,Int)], dir:String):List[(Int,Int)] = {
+  val hx = h._1
+  val hy = h._2
+  
+  val nh = dir match {
+    case "R" => (hx + 1, hy)
+    case "L" => (hx - 1, hy)
+    case "U" => (hx, hy + 1)
+    case "D" => (hx, hy - 1)
+  }
+
+  bringTailToHead(rope, List(nh))
+}
+
+def moveRope(input:List[(String,Int)], rope:List[(Int,Int)]): List[(Int,Int)] = {
+  val movement = input.headOption
+
+  movement match {
+    case None => rope
+    case Some((d, a))  => if (a>0) {
+      moveRope(List((d,a-1)) ++ input.tail, moveHead(rope.head, rope.tail, d))
+    } else {
+      moveRope(input.tail, rope)
+    }
+  }
+} 
+
+moveRope(input, initialRope)
+
+println(hist.toSet.size)
