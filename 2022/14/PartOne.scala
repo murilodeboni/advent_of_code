5f285ff02abe95f4b684859d49dc05567bb363ed
Murilo Deboni <murilodeboni@Murilos-MacBook-Pro.local>
2023-02-05 13:10:11 +0100

    basis working, snow mechanics wip

--- 2022/14/PartOne.scala
+++ 2022/14/PartOne.scala
@@ -1,16 +1,16 @@
-import PartOne.Map.findBoundariesMap
+import PartOne14.Map.printMap
 
 import scala.annotation.tailrec
 import scala.io.Source
 
-object PartOne extends App {
+object PartOne14 extends App {
 
   case class Coordinate(x: Int, y: Int, isRock: Boolean = false, isSource: Boolean = false, hasSnow: Boolean = false)
   object Coordinate {
     @tailrec
     def getAllPaths(input: List[Coordinate], result: List[Coordinate] = List()): List[Coordinate] = {
       if (input.tail.nonEmpty) {
-        getAllPaths(input.tail, getFullPath(input.head, input.tail.head))
+        getAllPaths(input.tail, result ++ getFullPath(input.head, input.tail.head))
       } else {
         result
       }
@@ -32,9 +32,10 @@ object PartOne extends App {
 
   case class Map(coordinates: List[Coordinate])
   object Map {
+      val source = Coordinate(500,0, isSource = true)
     def createMap(input: List[List[Coordinate]]): Map = {
-      val rocks = input.map(i => Coordinate.getAllPaths(i)).reduce(_ ++ _)
-      new Map(rocks)
+      val rocks = input.map(i => Coordinate.getAllPaths(i)).reduce(_ ++ _).distinct
+      fillMap(source +: rocks)
     }
 
     def findBoundariesMap(rocks: List[Coordinate]): (Int,Int,Int,Int) = {
@@ -43,11 +44,47 @@ object PartOne extends App {
         .reduce{ (a,b) => (a,b) match { case ((xl, xr, xb, xt), (yl, yr, yb, yt)) =>
           (xl min yl, xr max yr, xb min yb, xt max yt)
         }}
-
       boundaries match {
-        case (down, up, left, right) => (down -1, up, left - 1, right +1)
+        case (left, right, up, down) => (left-1, right+1, up, down+1)
       }
+    }
 
+    def printMap(map: Map): Unit = {
+      val sortedMap = map.coordinates.sortWith((c1,c2) => c1.x < c2.x).sortWith((c1,c2) => c1.y < c2.y)
+
+      var line: String = ""
+      var y = -1
+      sortedMap.foreach(
+        c => {
+          if (c.y > y) {
+            y = c.y
+            line = line + "\n"
+          }
+
+          (c.hasSnow, c.isRock, c.isSource) match {
+          case (true, _, _) => line = line + "o"
+          case (_, true, _) => line = line + "#"
+          case (_, _, true) => line = line + "+"
+          case _ => line = line + "."
+        }
+        }
+      )
+
+      println(line)
+    }
+
+    def fillMap(initialCoordinates: List[Coordinate]): Map = {
+      var otherCoordinates: List[Coordinate] = List()
+      val (left, right, up, down) = Map.findBoundariesMap(initialCoordinates)
+      (left to right).map(
+        ix => (up to down).map(
+          jy => initialCoordinates.find(c => c.x == ix && c.y == jy) match {
+            case None => otherCoordinates = otherCoordinates :+ Coordinate(ix, jy)
+            case Some(c) => // already exists
+          }
+        )
+      )
+      Map(initialCoordinates ++ otherCoordinates)
     }
   }
 
@@ -59,8 +96,29 @@ object PartOne extends App {
       Coordinate(c(0).trim.toInt, c(1).trim.toInt)
     }).toList)
 
+  def isFull(x: Int,y: Int, map: Map): Boolean = {
+    map.coordinates.find(c => c.x == x && c.y == y && (c.isRock || c.isSource || c.hasSnow)) match {
+      case None => false
+      case Some(c) => true
+    }
+  }
+
+  def putSnow(x: Int, y:Int, map: Map): Map = {
+    Map(map.coordinates.filter(c => c.x != x || c.y != y) :+ Coordinate(x = x, y = y, hasSnow = true))
+  }
+  def fallSnow(map: Map): Map = {
+    var (xi,yi) = (Map.source.x, Map.source.y)
+
+    while (!isFull(xi,yi+1,map)) {
+      yi = yi + 1
+    }
+
+    val newMap = putSnow(xi,yi,map)
+
+    printMap(newMap)
+    newMap
+  }
+
   val map = Map.createMap(inputText)
-
-  println(findBoundariesMap(map.coordinates))
-
+  fallSnow(map)
 }
