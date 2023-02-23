e89cfe3f3aa6df0b2a06bcd51769b400fa4f6012
Murilo Deboni <murilodeboni@Murilos-MacBook-Pro.local>
2023-02-23 23:47:16 +0100

    no algo implementation

--- 2022/16/PartOne16.scala
+++ 2022/16/PartOne16.scala
@@ -1,41 +1,17 @@
 import scala.annotation.tailrec
 import scala.io.Source
-import scala.math
 import scala.util.matching.Regex
 
 object PartOne16 extends App {
 
-  val timeLimit: Int = 30
-
-  case class Valve(code: String, rate: Int, leadsTo: List[String], timeOpened: Int = timeLimit, isOpened: Boolean = false)
-  case class Cave(valves: List[Valve], currentValve: Valve, time: Int = 0, rate: Int = 0) {
+  case class Valve(code: String, rate: Int, leadsTo: List[String])
+  case class Cave(valves: List[Valve], currentValve: Valve) {
     def findValve(code: String): Valve = {
       valves.find(_.code == code).getOrElse(throw new Exception(f"Valve $code not found"))
     }
 
-    def goTo(to: Valve, debug: Boolean): Cave = {
-      if (currentValve.leadsTo.contains(to.code)) {
-        if (debug) println(f"goign from ${currentValve} to ${to}")
-        Cave(valves, to, time + 1, rate)
-      } else {
-        throw new Exception(f"Movement not possible (from ${currentValve.code} to ${to.code})")
-      }
-    }
-
-    def getRate: Int = {
-      valves.filter(_.timeOpened < time).map(v => v.rate * (time - v.timeOpened)).sum
-    }
-
-    def openValve(debug: Boolean): Cave = {
-      if (currentValve.isOpened) {
-        throw new Exception("Valve already opened")
-      } else {
-        val newValve = currentValve.copy(timeOpened = time, isOpened = true)
-        val newValves = valves.filter(_.code != currentValve.code) :+ newValve
-
-        if (debug) println(f"opening ${currentValve}")
-        this.copy(valves = newValves, currentValve = newValve, time = time + 1, rate = getRate)
-      }
+    def removeValve(code: String): Cave = {
+      this.copy(valves = valves.filter(_.code != code))
     }
 
     // Floyd-Marshall
@@ -70,30 +46,13 @@ object PartOne16 extends App {
     }
   }
 
-  def algo(cave: Cave, debug: Boolean = false): Cave = {
-    if (debug) println(f"time: ${cave.time}, rate: ${cave.getRate}")
-    cave.time match {
-      case PartOne16.timeLimit => cave
-      case _ => if (cave.currentValve.isOpened) {
-        algo(cave.goTo(cave.findValve(cave.currentValve.leadsTo.head),  debug), debug)
-      } else {
-        algo(cave.openValve(debug), debug)
-      }
-    }
+  val listInput = loadInput("./2022/16/test_input.txt")
+  val valves = parseInput(listInput)
+  val cave = Cave(valves, valves.head)
+  val distances: Map[(String, String), Int] = cave.getInitialDistances
+  var possibleResults = List(0)
 
-  }
+  val s = valves.find(_.code == "AA")
 
-  def run(isTest: Boolean = false, input: String = "./2022/16/"): Unit = {
-    val listInput = loadInput(input + (if (isTest) "test_input.txt" else "input.txt"))
 
-    val valves = parseInput(listInput)
-
-    val cave = Cave(valves, valves.head)
-
-//    algo(cave, true)
-
-    println(cave.getInitialDistances)
-  }
-
-  run(true)
 }
