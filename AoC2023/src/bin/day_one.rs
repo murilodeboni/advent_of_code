ec0b44554da486886fdbe5fec82280ca2f3dd255
Murilo Deboni <murilodeboni@Murilos-MacBook-Air.local>
2023-12-08 23:58:44 +0100

    test input working

--- AoC2023/src/bin/day_one.rs
+++ AoC2023/src/bin/day_one.rs
@@ -1,3 +1,23 @@
+use regex::Regex;
+
 fn main() {
-    println!("Hello, day one!");
+    let input = [
+        "1abc2",
+        "pqr3stu8vwx",
+        "a1b2c3d4e5f",
+        "treb7uchet"
+    ];
+
+    let first_re = Regex::new(r"^[a-zA-Z]*(\d).*$").unwrap();
+    let last_re = Regex::new(r"^.*(\d)[a-zA-Z]*$").unwrap();
+
+    let mut ans = 0;
+
+    for i in input {
+        let Some(first_digit) = first_re.captures(i) else { return };
+        let Some(last_digit) = last_re.captures(i) else { return };
+        ans = &ans + [&first_digit[1],&last_digit[1]].join("").parse::<i32>().unwrap();
+        println!("{}", &ans);
+    }
+
 }
