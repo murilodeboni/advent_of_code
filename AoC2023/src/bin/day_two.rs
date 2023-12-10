173e6d0ed1e21753eec5bb064cc2e7d68c365361
Murilo Deboni <murilodeboni@Murilos-MacBook-Air.local>
2023-12-10 01:46:11 +0100

    part 2

--- AoC2023/src/bin/day_two.rs
+++ AoC2023/src/bin/day_two.rs
@@ -1,3 +1,4 @@
+
 use std::collections::HashMap;
 use regex::Regex;
 use std::fs::read_to_string;
@@ -5,41 +6,37 @@ use std::fs::read_to_string;
 fn main() {
     let input = read_lines("/Users/murilodeboni/advent_of_code/AoC2023/src/bin/day_two_input.txt");
 
-    let game_regex = Regex::new(r"^Game (\d+).*$").unwrap();
     let ball_regex = Regex::new(r"(\d+) (blue|green|red)").unwrap();
 
-    let mut possible = true;
-
-    let bag = HashMap::from([
-        ("red", 12),
-        ("green", 13),
-        ("blue", 14)
-    ]);
+    let mut bag = std::collections::HashMap::<String, i32>::new();
 
     let mut ans = 0;
 
     for i in input {
-        possible = true;
-        let game_number = &game_regex.captures(&i).unwrap()[1].parse::<i32>().unwrap();
+        bag.insert("red".to_string(), 0);
+        bag.insert("green".to_string(), 0);
+        bag.insert("blue".to_string(), 0);
 
         let pulls: Vec<_> = i.split(":").collect::<Vec<_>>()[1].split(";").collect();
 
         for pull in pulls {
             let balls: Vec<_> = pull.split(", ").collect();
             for ball in balls {
-                let b = &ball_regex.captures(&ball).unwrap();    
-                if bag.get(&b[2]).unwrap() < &b[1].parse::<i32>().unwrap() {
-                    possible = false;
-                    break;
+                let b = &ball_regex.captures(&ball).unwrap();
+                let v = &b[1].parse::<i32>().unwrap();
+                if bag.get(&b[2]).unwrap() < &v {   
+                    bag.insert((&b[2]).to_string(), *v);
                 }
             }
         }
-        if possible {
-            ans = ans + game_number;
-            println!("{}", game_number)
-        }
+
+        ans = ans + power(bag.clone())
     }
     println!("{}", ans)
+}
+
+fn power(bag: HashMap::<String, i32>) -> i32 {
+    bag.get("red").unwrap() * bag.get("blue").unwrap() * bag.get("green").unwrap()
 }
 
 fn read_lines(filename: &str) -> Vec<String> {
