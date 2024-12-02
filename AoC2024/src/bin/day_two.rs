6fd39ff98684166793645bd2698cabcd4295927a
Murilo Deboni <murilodeboni@Murilos-MacBook-Air.local>
2024-12-02 20:06:14 +0100

    part 2 working

--- AoC2024/src/bin/day_two.rs
+++ AoC2024/src/bin/day_two.rs
@@ -6,20 +6,22 @@ fn main() {
     let input = read_lines("./src/bin/inputs/day_two.txt");
 
     let mut cnt_part_one = 0;
+    let mut cnt_part_two = 0;
 
     for i in input {
         let levels_str: Vec<&str> = i.split(" ").collect();
 
         let levels: Vec<isize> = levels_str.iter().map(|l| l.parse::<isize>().expect("Failed to parse")).collect();
         
-        cnt_part_one += part1(&levels);
+        cnt_part_one += if part1(&levels) {1} else {0};
+        cnt_part_two += if part2(&levels) {1} else {0};
     }
 
     println!("{}", cnt_part_one);
-
+    println!("{}", cnt_part_two);
 }
 
-fn part1(levels: &Vec<isize>) -> usize {
+fn part1(levels: &Vec<isize>) -> bool {
     let mut is_safe: bool = true;
     let is_increasing = levels[1] > levels[0];
 
@@ -39,13 +41,22 @@ fn part1(levels: &Vec<isize>) -> usize {
         }
         i += 1;
     }
-
+    
     // println!("{:?} {} {}", levels, i, is_safe);
+    
+    is_safe
+}
 
+fn part2(levels: &Vec<isize>) -> bool {
+    let s: usize = levels.len();
 
-    if is_safe {
-        return 1;
-    } else {
-        return 0;
+    for i in 0..s {
+        let left_part= levels[..i].to_vec();
+        let right_part = levels[i + 1..].to_vec();
+        
+        if part1(&[left_part, right_part].concat()) {
+            return true;
+        }
     }
+    return false;
 }
