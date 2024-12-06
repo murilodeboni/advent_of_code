cc1f9bbb618a9fcba054d081f13a7a59fc01bc91
Murilo Deboni <murilodeboni@Murilos-MacBook-Air.local>
2024-12-06 14:23:32 +0100

    part 1 working

--- AoC2024/src/bin/day_06.rs
+++ AoC2024/src/bin/day_06.rs
@@ -2,23 +2,19 @@ mod utils;
 
 use utils::input::read_lines;
 
-use std::{collections::HashMap, hash::Hash};
+use std::collections::HashMap;
 
 fn main() {
-    let input = read_lines("./src/bin/inputs/day_06_test.txt");
+    let input = read_lines("./src/bin/inputs/day_06.txt");
 
-    let mut part1Ans: usize = 0;
-    let mut part2Ans: usize = 0;
-
-    let mut direction: usize = 0;
-    let mut go_map: HashMap<usize,(i8,i8)> = HashMap::new();
-    go_map.insert(0, (-1,0));
-    go_map.insert(1, (0,1));
-    go_map.insert(2, (1,0));
-    go_map.insert(3, (0,-1));
-
+    let part_1_ans: usize;
+    
+    let direction: String = "up".to_string();
+    
     let mut matrix:Vec<Vec<String>> = Vec::new();
     let mut position: (usize, usize) = (0,0);
+    
+    let mut positions_map: HashMap<(usize,usize), bool> = HashMap::new();
     
     let mut c = 0;
     for i in input {
@@ -30,55 +26,77 @@ fn main() {
         c += 1
     }
 
-    part1Ans += go(direction, go_map, &matrix, position);
+    positions_map.insert(position, true);
+    part_1_ans = go(direction, position, &matrix, positions_map);
 
-    println!("{}", part1Ans);
-    println!("{}", part2Ans);
-
+    println!("{}", part_1_ans);
 }
 
-fn go(mut direction: usize, go_map: HashMap<usize,(i8,i8)>, matrix: &Vec<Vec<String>>, mut position: (usize, usize)) -> usize {
-    let max_y = matrix.len();
-    let max_x = matrix[0].len();
+fn go(direction: String, position: (usize, usize), matrix: &Vec<Vec<String>>, mut positions_map: HashMap<(usize,usize), bool>) -> usize {
+    // print_matrix(position, matrix);
+    let new_position: (usize, usize);
 
-    let mut positions_map: HashMap<(usize,usize), usize> = HashMap::new();
-
-    while true {
-        positions_map.insert(position, 1);
-        if let Some(to) = go_map.get(&direction) {
-            let np = (position.0 as isize + to.0 as isize, position.1 as isize + to.1 as isize);
-            if np.0 >= 0 && np.1 >= 0 && np.0 < max_x as isize && np.1 < max_y as isize {
-                let np = (np.0 as usize, np.1 as usize);
-                let nd = is_blocked(&matrix, np, direction);
-                if nd == direction {
-                    position = np;
-                }
-            } else {
-                return positions_map.len()
-            }
+    if direction == "up" {
+        if position.0 >= 1 {
+            new_position = (position.0 - 1, position.1);
+        } else {
+            print_matrix(position, matrix);
+            return positions_map.len();
+        }
+    } else if direction == "right" {
+        if position.1 < matrix[0].len() - 1 {
+            new_position = (position.0 , position.1 + 1);
+        } else {
+            print_matrix(position, matrix);
+            return positions_map.len();
+        }
+    } else if direction == "down" {
+        if position.0 < matrix.len() - 1 {
+            new_position = (position.0 + 1, position.1);
+        } else {
+            print_matrix(position, matrix);
+            return positions_map.len();
+        }
+    } else {
+        if position.0 >= 1 {
+            new_position = (position.0, position.1 - 1);
+        } else {
+            print_matrix(position, matrix);
+            return positions_map.len();
         }
     }
+    
+    let blocked = is_blocked(new_position, matrix);
 
-    positions_map.len()
+    if blocked {
+        return go(turn(direction), position, matrix, positions_map);
+    } else {
+        positions_map.insert(new_position, true);
+        return go(direction, new_position, matrix, positions_map);
+    }
 }
 
-fn is_blocked(matrix: &Vec<Vec<String>>, position: (usize, usize), direction: usize) -> usize {
+fn is_blocked(position: (usize, usize), matrix: &Vec<Vec<String>>) -> bool {
+    matrix[position.0][position.1] == "#"
+}
+
+fn turn(direction: String) -> String {
+    if direction == "up" {
+        return "right".to_string();
+    } else if direction == "right" {
+        return "down".to_string();
+    } else if direction == "down" {
+        return "left".to_string();
+    } else {
+        return "up".to_string();
+    }
+}
+
+fn print_matrix(position: (usize, usize), matrix: &Vec<Vec<String>>) -> () {
     let mut nm = matrix.clone();
     nm[position.0][position.1] = "K".to_string();
-    
-    println!("=============");
-
-    if matrix[position.0][position.1] == "#" {
-        turn(direction)
-    } else {
-        direction
+    for m in nm {
+        println!("{:?}", m.concat());
     }
-}
-
-fn turn(direction: usize) -> usize {
-    if direction == 3 {
-        0
-    } else {
-        direction + 1
-    }
+    println!("================");
 }
