3ff3d56144247f1390e0ac5314447005f399714f
Murilo Deboni <murilodeboni@Murilos-MacBook-Air.local>
2024-12-07 10:15:34 +0100

    add time to calculate 202 ms both parts

--- AoC2024/src/bin/day_07.rs
+++ AoC2024/src/bin/day_07.rs
@@ -1,11 +1,16 @@
 mod utils;
 
+use std::time::Instant;
+
 use utils::input::read_lines;
 
 fn main() {
     let input = read_lines("./src/bin/inputs/day_07.txt");
+    let mut part1_ans:usize = 0;
     let mut part2_ans:usize = 0;
-    
+
+    let start = Instant::now();
+
     for i in input {
         let mut right: Vec<usize> = Vec::new();
 
@@ -17,11 +22,16 @@ fn main() {
             right.push(value.parse().expect("error"));
         }
         
-        let part2_case = part1(left, right, Vec::new());
-        
-        part2_ans += part2_case;
+        part1_ans += part1(left, right.clone(), Vec::new());
+        part2_ans += part2(left, right, Vec::new());
+
     }
+
+    let final_time = start.elapsed();
+
+    println!("{}", part1_ans);
     println!("{}", part2_ans);
+    println!("{}", final_time.as_millis())
 }
 
 fn part1(left: usize, mut right: Vec<usize>, possbilities: Vec<usize>) -> usize {
@@ -29,6 +39,37 @@ fn part1(left: usize, mut right: Vec<usize>, possbilities: Vec<usize>) -> usize 
         let v = right[0];
         if possbilities.is_empty() {
             return part1(left, right.split_off(1) , vec![v]);
+        } else {
+            let mut new_possibilities: Vec<usize> = Vec::new();
+            for p in &possbilities {
+
+                let new_v_mult: usize = p*v;
+                let new_v_sum: usize = p+v;
+
+                if new_v_mult <= left {
+                    new_possibilities.push(new_v_mult);
+                }
+
+                if new_v_sum <= left {
+                    new_possibilities.push(new_v_sum);
+                }
+
+            }
+            if new_possibilities.is_empty() {
+                return 0;
+            }
+
+            return part1(left, right.split_off(1) , new_possibilities);
+        }
+    }
+    return if possbilities.contains(&left) {left} else {0};
+}
+
+fn part2(left: usize, mut right: Vec<usize>, possbilities: Vec<usize>) -> usize {
+    while !right.is_empty() {
+        let v = right[0];
+        if possbilities.is_empty() {
+            return part2(left, right.split_off(1) , vec![v]);
         } else {
             let mut new_possibilities: Vec<usize> = Vec::new();
             for p in &possbilities {
@@ -53,7 +94,7 @@ fn part1(left: usize, mut right: Vec<usize>, possbilities: Vec<usize>) -> usize 
                 return 0;
             }
 
-            return part1(left, right.split_off(1) , new_possibilities);
+            return part2(left, right.split_off(1) , new_possibilities);
         }
     }
     return if possbilities.contains(&left) {left} else {0};
