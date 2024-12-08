b480c7998f5d15f16c76a53d338e62a749ba9956
Murilo Deboni <murilodeboni@Murilos-MacBook-Air.local>
2024-12-08 09:01:54 +0100

    day 08 boileplate

---
+++ AoC2024/src/bin/day_08.rs
@@ -0,0 +1,14 @@
+mod utils;
+
+use utils::input::read_lines;
+
+use std::collections::HashMap;
+
+use std::time::Instant;
+
+fn main() {
+    let start = Instant::now();
+    let input = read_lines("./src/bin/inputs/day_08_test.txt");
+
+    let part_1_ans: usize;
+}
