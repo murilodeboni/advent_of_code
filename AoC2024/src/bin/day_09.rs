5987b08d5528eca81733ca325f478758e8f8c5d1
Murilo Deboni <murilodeboni@Murilos-MacBook-Air.local>
2024-12-09 21:38:59 +0100

    organize part 1

--- AoC2024/src/bin/day_09.rs
+++ AoC2024/src/bin/day_09.rs
@@ -12,15 +12,10 @@ fn main() {
         .filter_map(|c| c.to_digit(10).map(|d| d as usize))
         .collect();
 
-    println!("{:?} {}", part1(disk_map), {start.elapsed().as_millis()});
-
-
-}
-
-fn part1(disk_map: Vec<usize>) -> usize {
+    
     let mut file_size: Vec<(usize, usize)> = Vec::new();
     let mut free_space: Vec<usize> = Vec::new();
-
+    
     for i in 0..disk_map.len() {
         if i%2==0 {
             file_size.push((i/2,disk_map[i]));
@@ -28,19 +23,22 @@ fn part1(disk_map: Vec<usize>) -> usize {
             free_space.push(disk_map[i]);
         }
     }
-
+    
     let mut final_vec: Vec<Option<usize>> = Vec::new();
 
     for i in 0..free_space.len() {
         final_vec.append(&mut vec![Some(file_size[i].0); file_size[i].1]);
         final_vec.append(&mut vec![None; free_space[i]]);
     }
+
     final_vec.append(&mut vec![Some(file_size[file_size.len()-1].0); file_size[file_size.len()-1].1]);
 
-    calculate_part_1_ans(custom_sort(final_vec))
+    println!("{:?} {}", calculate_ans(custom_sort_part_1(final_vec)), {start.elapsed().as_millis()});
+    
 }
 
-fn custom_sort(v: Vec<Option<usize>>) -> Vec<usize> {
+
+fn custom_sort_part_1(v: Vec<Option<usize>>) -> Vec<usize> {
     // println!("vector - {:?}", v);
     let mut sv: Vec<usize> = Vec::new();
     let mut i = 0;
@@ -62,7 +60,7 @@ fn custom_sort(v: Vec<Option<usize>>) -> Vec<usize> {
     sv
 }
 
-fn calculate_part_1_ans(v: Vec<usize>) -> usize {
+fn calculate_ans(v: Vec<usize>) -> usize {
     let mut ans: usize = 0;
     for i in 0..v.len() {
         ans += i*v[i];
