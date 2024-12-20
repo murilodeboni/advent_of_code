6256eb3c7ed2316f5a97372a7cc6f627cd5e7c8e
Murilo Deboni <murilodeboni@Murilos-MacBook-Air.local>
2024-12-21 00:07:45 +0100

    part 1 working

--- AoC2024/src/bin/day_19.rs
+++ AoC2024/src/bin/day_19.rs
@@ -2,7 +2,7 @@ mod utils;
 
 use utils::input::read_lines;
 
-use std::time::Instant;
+use std::{collections::HashMap, time::Instant};
 
 
 fn main() {
@@ -11,27 +11,35 @@ fn main() {
     let patterns: Vec<&str> = input[0].split(", ").collect();
     let input2= read_lines("./src/bin/inputs/day_19_2.txt");
     let towels: Vec<&str> = input2.iter().map(|s| s.as_str()).collect();
+    // let towels = vec!["uwgbwwgwwgwrwwbrruubuugrgrrwbwburgbguuugwrwwgrbbubwwgrbb"];
+    let mut memo = &mut HashMap::new();
     let mut part1: usize = 0;
 
     for towel in towels {
         println!("trying to form '{}' ", towel);
-        if is_possible(towel, &patterns) {part1 += 1}
+        if is_possible(towel, &patterns, &mut memo) {part1 += 1}
     }
     println!("part 1 - {} took {}ms", part1, start_time.elapsed().as_millis());
 }
 
-fn is_possible(target: &str, elements: &Vec<&str>) -> bool {
-    // println!("remaining {}", target);
+fn is_possible(target: &str, elements: &Vec<&str>, memo: &mut HashMap<String, bool>) -> bool {
+    // println!("memory size {}", memo.len());
+    if let Some(&result) = memo.get(target) {
+        return result;
+    }
+
     if target.is_empty() {
         return true;
     }
     for &element in elements {
         if target.starts_with(element) {
             let remaining = &target[element.len()..];
-            if is_possible(remaining, elements) {
+            if is_possible(remaining, elements, memo) {
+                memo.insert(target.to_string(), true);
                 return true;
             }
         }
     }
+    memo.insert(target.to_string(), false);
     false
 }
