35cb28b49194ad867e232b20562cc597d7538b97
Murilo Deboni <murilodeboni@Murilos-MacBook-Air.local>
2024-12-21 00:20:56 +0100

    part 2 working 350ms

--- AoC2024/src/bin/day_19.rs
+++ AoC2024/src/bin/day_19.rs
@@ -11,35 +11,39 @@ fn main() {
     let patterns: Vec<&str> = input[0].split(", ").collect();
     let input2= read_lines("./src/bin/inputs/day_19_2.txt");
     let towels: Vec<&str> = input2.iter().map(|s| s.as_str()).collect();
-    // let towels = vec!["uwgbwwgwwgwrwwbrruubuugrgrrwbwburgbguuugwrwwgrbbubwwgrbb"];
     let mut memo = &mut HashMap::new();
     let mut part1: usize = 0;
+    let mut part2: usize = 0;
 
     for towel in towels {
         println!("trying to form '{}' ", towel);
-        if is_possible(towel, &patterns, &mut memo) {part1 += 1}
+        let ans = is_possible(towel, &patterns, &mut memo);
+        if ans > 0 {
+            part1 += 1;
+            part2 += ans;
+        }
     }
-    println!("part 1 - {} took {}ms", part1, start_time.elapsed().as_millis());
+
+    println!("part 1 - {}, part 2 - {} | took {}ms", part1, part2, start_time.elapsed().as_millis());
 }
 
-fn is_possible(target: &str, elements: &Vec<&str>, memo: &mut HashMap<String, bool>) -> bool {
-    // println!("memory size {}", memo.len());
+fn is_possible(target: &str, elements: &Vec<&str>, memo: &mut HashMap<String, usize>) -> usize {
     if let Some(&result) = memo.get(target) {
         return result;
     }
 
     if target.is_empty() {
-        return true;
+        return 1;
     }
+
+    let mut total_ways = 0;
+
     for &element in elements {
         if target.starts_with(element) {
             let remaining = &target[element.len()..];
-            if is_possible(remaining, elements, memo) {
-                memo.insert(target.to_string(), true);
-                return true;
-            }
+            total_ways += is_possible(remaining, elements, memo);
         }
     }
-    memo.insert(target.to_string(), false);
-    false
+    memo.insert(target.to_string(), total_ways);
+    total_ways
 }
