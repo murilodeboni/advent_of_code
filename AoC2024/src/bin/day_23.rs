2bb3d9461c373adcd611b771e9ddaf5be7a1a1f3
Murilo Deboni <murilodeboni@Murilos-MacBook-Air.local>
2024-12-28 22:55:15 +0100

    part2 cheating with chatgpt

--- AoC2024/src/bin/day_23.rs
+++ AoC2024/src/bin/day_23.rs
@@ -16,18 +16,28 @@ fn main() {
         connections.entry(pcs[1].to_string()).or_insert(HashSet::new()).insert(pcs[0].to_string());
     }
 
-    let mut part1: usize  = part1(connections);
+    let mut part1: usize  = part1(&connections);
 
     println!("part 1 - {} took {}ms", part1, start_time.elapsed().as_millis());
+
+    let mut network  = part2(&connections);
+    let mut part2: Vec<String> = Vec::new();
+    for c in network {
+        part2.push(c);
+    }
+    part2.sort();
+
+    println!("part 2 - {} took {}ms", part2.join(","), start_time.elapsed().as_millis());
+
 }
 
-fn part1(connections: HashMap<String, HashSet<String>>) -> usize {
+fn part1(connections: &HashMap<String, HashSet<String>>) -> usize {
     let mut part1: usize  = 0;
 
     let mut three_way_connections: HashSet<Vec<String>> = HashSet::new();
 
     // the computer
-    for (computer, neighbors) in &connections {
+    for (computer, neighbors) in connections {
         // first connection
         for neighbor in neighbors {
             let mut nn = connections.get(neighbor).unwrap();
@@ -53,6 +63,50 @@ fn part1(connections: HashMap<String, HashSet<String>>) -> usize {
     }
 
     // println!("{:?}", three_way_connections);
-
+    
     part1
+}
+
+fn part2(graph: &HashMap<String, HashSet<String>>) -> HashSet<String> {
+    let mut cliques = Vec::new();
+    let mut p: HashSet<_> = graph.keys().cloned().collect();
+    let mut r = HashSet::new();
+    let mut x = HashSet::new();
+
+    bron_kerbosch(graph, &mut r, &mut p, &mut x, &mut cliques);
+
+    // Find the largest clique
+    cliques.into_iter().max_by_key(|clique| clique.len()).unwrap_or_default()
+}
+
+fn bron_kerbosch(
+    graph: &HashMap<String, HashSet<String>>,
+    r: &mut HashSet<String>,
+    p: &mut HashSet<String>,
+    x: &mut HashSet<String>,
+    cliques: &mut Vec<HashSet<String>>,
+) {
+    if p.is_empty() && x.is_empty() {
+        // Found a maximal clique
+        cliques.push(r.clone());
+    } else {
+        let pivot = p.union(x).next().unwrap().clone(); // Choose a pivot
+        let mut p_without_neighbors: HashSet<_> = p.difference(&graph[&pivot]).cloned().collect();
+
+        for v in p_without_neighbors {
+            // Add v to the current clique
+            r.insert(v.clone());
+
+            // Recur with neighbors of v
+            let neighbors = &graph[&v];
+            let mut p_intersection: HashSet<_> = p.intersection(neighbors).cloned().collect();
+            let mut x_intersection: HashSet<_> = x.intersection(neighbors).cloned().collect();
+            bron_kerbosch(graph, r, &mut p_intersection, &mut x_intersection, cliques);
+
+            // Backtrack
+            r.remove(&v);
+            p.remove(&v);
+            x.insert(v);
+        }
+    }
 }
