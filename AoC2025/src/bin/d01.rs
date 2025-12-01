20afaa41d3ffef8d587930cdbe3c6d0fed958726
Murilo Deboni <murilo.deboni@gmail.com>
2025-12-01 21:30:11 +0100

    day 1 solved

--- AoC2025/src/bin/d01.rs
+++ AoC2025/src/bin/d01.rs
@@ -3,28 +3,79 @@ use aoc_utils::read_input;
 const BASE: &str = env!("CARGO_MANIFEST_DIR");
 const DAY: &str = "d01";
 
-fn main() {
-    let input = read_input(BASE, DAY);
-    println!("{DAY} part1: {}", part1(&input));
-    println!("{DAY} part2: {}", part2(&input));
+struct DirectionInput {
+    direction: char,
+    value: i64,
 }
 
-fn part1(_input: &[String]) -> i64 {
-    0
-}
+struct Dial {
+    position: i64,
+    part1: i64,
+    part2: i64,
+} 
 
-fn part2(_input: &[String]) -> i64 {
-    0
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn smoke_test() {
-        let input = read_input(BASE, DAY);
-        assert_eq!(part1(&input), 0);
-        assert_eq!(part2(&input), 0);
+impl Dial {
+    fn new() -> Self {
+        Dial { position: 50 , part1: 0, part2: 0 }
     }
+
+    fn turn1(&mut self, direction: char, value: i64) {
+        let initial_position = self.position.clone();
+        let rest = value % 100;
+        let full_turns = value / 100;
+        self.part2 += full_turns;
+        
+        match direction {
+            'L' => self.position -= rest,
+            'R' => self.position += rest,
+            _ => panic!("Invalid direction"),
+        }
+
+        if self.position < 0 {
+            self.position = 100 + self.position;
+            if initial_position != 0 {
+                self.part2 += 1;
+            }
+        } 
+        
+        if self.position > 99{
+            self.position = self.position - 100;
+            if self.position != 0 {
+                self.part2 += 1;
+            }
+        }
+
+        if self.position == 0 && rest > 0 {
+            self.part1 += 1;
+            self.part2 += 1;
+        }
+    }
+
+    fn print_position(&self) {
+        println!("Dial position: {}, cnt {}", self.position, self.part2);
+    }
+}
+
+fn parse_input(input: &[String]) -> Vec<DirectionInput> {
+    input.iter().map(|s| {
+        let (dir, val) = s.split_at(1);
+        DirectionInput {
+            direction: dir.chars().next().unwrap(),
+            value: val.parse().unwrap(),
+        }
+    }).collect()
+}
+
+fn main() {
+    let input = read_input(BASE, DAY, false);
+
+    let dir_input: Vec<DirectionInput> = parse_input(&input);
+    
+    let mut dial = Dial::new();
+    dir_input.iter().for_each(|di| {
+        dial.turn1(di.direction, di.value);
+    });
+
+    println!("{DAY} part1: {}", dial.part1);
+    println!("{DAY} part2: {}", dial.part2);
 }
